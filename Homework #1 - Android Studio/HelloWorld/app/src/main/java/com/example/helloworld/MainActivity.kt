52c1a17ca4f4325292757eed0671bf8a42b2dbd0
Dyrits <dyrits@outlook.com>
2020-11-10 17:27:21 +0300

    Kotlin Extension.

--- Homework #1 - Android Studio/HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.kt
+++ Homework #1 - Android Studio/HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -4,23 +4,18 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.widget.EditText
-import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
-    lateinit var textView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        textView = findViewById(R.id.text_view)
     }
 
     fun onClick(view: View) {
-        val anything = findViewById<EditText>(R.id.input_text)
-        val textViewText = if (anything.text.toString() != "") anything.text.toString() else "Hello World!"
-        textView.text = textViewText
+        val textViewText = if (input_text.text.toString() != "") input_text.text.toString() else "Hello World!"
+        text_view.text = textViewText
         Log.d("MainActivity", textViewText)
     }
 }
